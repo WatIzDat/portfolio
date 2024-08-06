c87af0f2082aca8e54fdd0b67da39b87ba72583b
WatIzDat <lily3575@hotmail.com>
2024-08-06 14:03:32 -0700

    Get upload form working using fork

--- portfolio-manager/src/portfolio_manager/events.cljs
+++ portfolio-manager/src/portfolio_manager/events.cljs
@@ -1,10 +1,10 @@
 (ns portfolio-manager.events
-  (:require
-   [re-frame.core :as re-frame]
-   [portfolio-manager.db :as db]
-   ["quill$default" :as q]
-   [portfolio-manager.consts :as consts]
-   [superstructor.re-frame.fetch-fx]))
+  (:require ["quill$default" :as q]
+            [fork.re-frame :as fork]
+            [portfolio-manager.consts :as consts]
+            [portfolio-manager.db :as db]
+            [re-frame.core :as re-frame]
+            [superstructor.re-frame.fetch-fx]))
 
 (re-frame/reg-event-db
  ::initialize-db
@@ -13,23 +13,26 @@
 
 (re-frame/reg-event-db
  ::upload-success
- (fn [_ _]
-   (println "Success!")))
+ (fn [db [_ path]]
+   (println "Success!")
+   (fork/set-submitting db path false)))
 
 (re-frame/reg-event-fx
  ::upload
- (fn [_ _]
+ (fn [{db :db} [_ {:keys [values _ path]}]]
    (let [editor (.querySelector js/document consts/editor-id)
          delta (js->clj (.stringify js/JSON (.getContents (q/find editor))))]
-     {:fx [[:fetch {:method :post
+     (println (values "id"))
+     {:db (fork/set-submitting db path true)
+      :fx [[:fetch {:method :post
                     :url "http://localhost:3001/api/article"
                     :request-content-type :json
                     :headers {"Accept" "application/json"
                               "Origin" "http://localhost:8280"}
-                    :body {:id "5"
-                           :name "Upload Test"
+                    :body {:id (values "id")
+                           :name (values "name")
                            :markdown delta
-                           :project-completion 50}
+                           :project-completion (parse-long (values "project-completion"))}
                     :mode :cors
                     :credentials :omit
-                    :on-success [::upload-success]}]]})))
+                    :on-success [::upload-success path]}]]})))
--- portfolio-manager/src/portfolio_manager/views.cljs
+++ portfolio-manager/src/portfolio_manager/views.cljs
@@ -1,33 +1,57 @@
 (ns portfolio-manager.views
   (:require [portfolio-manager.events :as events]
-            [re-frame.core :as re-frame]))
+            [re-frame.core :as re-frame]
+            [fork.re-frame :as fork]))
 
 (defn main-panel []
   [:div.flex.flex-col.justify-center.items-center.h-screen
    [:div {:class "size-2/3 mb-24"}
-    [:div.flex
-     [:div.flex.flex-col.mr-4
-      [:label {:for "id"} "ID:"]
-      [:input.border.rounded-lg.border-gray-400.mb-4
-       {:type "text"
-        :name "id"
-        :id "id"}]]
-     [:div.flex.flex-col.mr-4
-      [:label {:for "name"} "Name:"]
-      [:input.border.rounded-lg.border-gray-400.mb-4
-       {:type "text"
-        :name "name"
-        :id "name"}]]
-     [:div.flex.flex-col
-      [:label {:for "project-completion"} "Project Completion (%):"]
-      [:input.border.rounded-lg.border-gray-400.mb-4
-       {:type "text"
-        :name "project-completion"
-        :id "project-completion"}]]]
-    [:div {:id "editor"}
-     [:p "Hello World"]]
-    [:div.flex.flex-row-reverse
-     [:button.bg-blue-500.text-white.px-4.py-2.rounded-lg.mt-4
-      {:on-click
-       #(re-frame/dispatch [::events/upload])}
-      "Upload"]]]])
+    [fork/form {:path [:form]
+                :form-id "form"
+                :prevent-default? true
+                :clean-on-unmount? true
+                :on-submit #(re-frame/dispatch [::events/upload %])}
+     (fn [{:keys [values
+                  form-id
+                  handle-change
+                  handle-blur
+                  submitting?
+                  handle-submit]}]
+       [:form.flex.flex-col.size-full
+        {:id form-id
+         :on-submit handle-submit}
+        [:div.flex
+         [:div.flex.flex-col.mr-4
+          [:label {:for "id"} "ID:"]
+          [:input.border.rounded-lg.border-gray-400.mb-4
+           {:type "text"
+            :name "id"
+            :id "id"
+            :on-change handle-change
+            :on-blur handle-blur
+            :value (values "id")}]]
+         [:div.flex.flex-col.mr-4
+          [:label {:for "name"} "Name:"]
+          [:input.border.rounded-lg.border-gray-400.mb-4
+           {:type "text"
+            :name "name"
+            :id "name"
+            :on-change handle-change
+            :on-blur handle-blur
+            :value (values "name")}]]
+         [:div.flex.flex-col
+          [:label {:for "project-completion"} "Project Completion (%):"]
+          [:input.border.rounded-lg.border-gray-400.mb-4
+           {:type "text"
+            :name "project-completion"
+            :id "project-completion"
+            :on-change handle-change
+            :on-blur handle-blur
+            :value (values "project-completion")}]]]
+        [:div {:class "size-full" :id "editor"}
+         [:p "Hello World"]]
+        [:div.flex.flex-row-reverse
+         [:button.bg-blue-500.text-white.px-4.py-2.rounded-lg.mt-4
+          {:type "submit"
+           :disabled submitting?}
+          "Upload"]]])]]])
