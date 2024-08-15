8f616f813c183525915996f9c4e9fbaa088695de
WatIzDat <lily3575@hotmail.com>
2024-08-14 21:06:00 -0700

    Add existence check in API handlers

--- portfolio-api/src/portfolio_api/handlers/article.clj
+++ portfolio-api/src/portfolio_api/handlers/article.clj
@@ -4,16 +4,23 @@
             [honey.sql :as sql]))
 
 (defn create [{{{:keys [id name markdown project-completion listed]} :body} :parameters}]
-  (jdbc/execute!
-   db
-   (sql/format {:insert-into [:articles]
-                :values [{:id id
-                          :name name
-                          :markdown markdown
-                          :project-completion project-completion
-                          :listed listed}]}))
-  {:status 200
-   :body id})
+  (let [exists (jdbc/execute-one!
+                db
+                (sql/format
+                 {:select 1
+                  :from [:articles]
+                  :where [:= :id id]}))]
+    (when (nil? exists)
+      (jdbc/execute!
+       db
+       (sql/format {:insert-into [:articles]
+                    :values [{:id id
+                              :name name
+                              :markdown markdown
+                              :project-completion project-completion
+                              :listed listed}]})))
+    {:status (if (nil? exists) 200 409)
+     :body id}))
 
 (defn get-all-no-markdown [_]
   {:status 200
@@ -23,33 +30,49 @@
                        :from [:articles]}))})
 
 (defn get-by-id [{{{:keys [id]} :path} :parameters}]
-  {:status 200
-   :body (jdbc/execute-one!
-          db
-          (sql/format {:select [:name :markdown :project-completion :listed]
-                       :from [:articles]
-                       :where [:= :id id]}))})
+  (let [article (jdbc/execute-one!
+                 db
+                 (sql/format {:select [:name :markdown :project-completion :listed]
+                              :from [:articles]
+                              :where [:= :id id]}))]
+    (if (nil? article)
+      {:status 404}
+      {:status 200
+       :body article})))
 
 (defn delete [{{{:keys [id]} :path} :parameters}]
-  (jdbc/execute!
-   db
-   (sql/format {:delete-from [:articles]
-                :where [:= :id id]}))
-  {:status 204})
+  (let [exists (jdbc/execute-one!
+                db
+                (sql/format
+                 {:select 1
+                  :from [:articles]
+                  :where [:= :id id]}))]
+    (when exists
+      (jdbc/execute!
+       db
+       (sql/format {:delete-from [:articles]
+                    :where [:= :id id]})))
+    {:status (if exists 204 404)}))
 
 (defn edit [{{{:keys [id name markdown project-completion listed]} :body
               {prev-id :id} :path} :parameters}]
-  (println prev-id)
-  (jdbc/execute!
-   db
-   (sql/format {:update :articles
-                :set {:id id
-                      :name name
-                      :markdown markdown
-                      :project-completion project-completion
-                      :listed listed}
-                :where [:= :id prev-id]}))
-  {:status 204})
+  (let [exists (jdbc/execute-one!
+                db
+                (sql/format
+                 {:select 1
+                  :from [:articles]
+                  :where [:= :id prev-id]}))]
+    (when exists
+      (jdbc/execute!
+       db
+       (sql/format {:update :articles
+                    :set {:id id
+                          :name name
+                          :markdown markdown
+                          :project-completion project-completion
+                          :listed listed}
+                    :where [:= :id prev-id]})))
+    {:status (if exists 204 404)}))
 
 (comment
   (jdbc/execute!
