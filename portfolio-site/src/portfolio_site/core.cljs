1ba4dce0588ca908cd0f1263ddcc66879290b2b1
WatIzDat <lily3575@hotmail.com>
2024-08-31 12:49:16 -0700

    Add shadow-cljs and rum to new project

---
+++ portfolio-site/src/portfolio_site/core.cljs
@@ -0,0 +1,14 @@
+(ns portfolio-site.core
+  (:require [rum.core :as rum]))
+
+(rum/defc heading []
+  [:<>
+   [:h1 "another testa1231asdf"]
+   [:h2 "yipee i think this finally works"]
+   [:p "test"]])
+
+(defn ^:dev/after-load mount-root []
+  (rum/mount (heading) (.getElementById js/document "app")))
+
+(defn init []
+  (mount-root))
