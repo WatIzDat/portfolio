31952276f798dcc584819a05887fcf2ce4deeeee
WatIzDat <lily3575@hotmail.com>
2024-09-01 16:01:23 -0700

    Set up basic static site using stasis

--- portfolio-site/project.clj
+++ portfolio-site/project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [stasis "2023.11.21"]
+                 [ring/ring-core "1.12.2"]
+                 [hiccup "2.0.0-RC3"]]
+  :ring {:handler portfolio-site.core/app}
+  :profiles {:dev {:plugins [[lein-ring "0.12.5"]]}}
   :repl-options {:init-ns portfolio-site.core})
--- portfolio-site/src/portfolio_site/core.clj
+++ portfolio-site/src/portfolio_site/core.clj
@@ -1,6 +1,25 @@
-(ns portfolio-site.core)
+(ns portfolio-site.core
+  (:require [stasis.core :as stasis]
+            [hiccup.page :refer [html5]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn layout-page [page]
+  (html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport"
+            :content "width=device-width, initial-scale=1.0"}]
+    [:title "Portfolio"]]
+   [:body
+    [:h1 "Test"]
+    [:div {:id "body"} page]]))
+
+(defn partial-pages [pages]
+  (zipmap (keys pages)
+          (map layout-page (vals pages))))
+
+(defn get-pages []
+  (stasis/merge-page-sources
+   {:public (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
+    :partials (partial-pages (stasis/slurp-directory "resources/partials" #".*\.html$"))}))
+
+(def app (stasis/serve-pages get-pages))
