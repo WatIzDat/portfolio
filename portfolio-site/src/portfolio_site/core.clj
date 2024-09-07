a2b80ab5679bd97ebb4211b73717f4bff638d9da
WatIzDat <lily3575@hotmail.com>
2024-09-07 16:19:30 -0700

    Add work-in-progress list and article previews

--- portfolio-site/src/portfolio_site/core.clj
+++ portfolio-site/src/portfolio_site/core.clj
@@ -4,11 +4,6 @@
             [stasis.core :as stasis]
             [cheshire.core :as cheshire]))
 
-(def page-data
-  (let [dirs (seq (.list (io/file "./resources/partials/")))]
-    (zipmap dirs
-            (map #(slurp (str "./resources/partials/" % "/data.json")) dirs))))
-
 (defn layout-page [page]
   (html5
    [:head
@@ -27,7 +22,8 @@
   (zipmap (keys pages)
           (map layout-page (vals pages))))
 
-(defn index-page [_]
+(defn index-page [page-data pages]
+  (println pages)
   (html5
    [:head
     [:meta {:charset "utf-8"}]
@@ -43,20 +39,40 @@
       [:h2 "Completed Projects"]
       [:ul.list-none.ml-0.border-4.border-yellow-50.rounded-lg.p-4.flex.flex-col.gap-4
        (->> page-data
-            (map #(first {(key %) (cheshire/parse-string (val %) true)}))
             (filter #(= (:project-completion (val %)) 100))
             (map
              (fn [x]
                [:li
                 [:button.bg-slate-800.py-4.rounded-lg.w-full
                  {:onclick (format "window.location.href='/%s'" (key x))}
-                 [:h3.text-center.m-0 (:name (val x))]]])))]]]]))
+                 [:h3.text-center.m-0 (:name (val x))]
+                 [:div.preview.line-clamp-2.overflow-auto
+                  {:inert ""}
+                  (pages (format "/%s/index.html" (key x)))]]])))]
+
+      [:h2.mt-8 "Work-in-progress Projects"]
+      [:ul.list-none.ml-0.border-4.border-yellow-50.rounded-lg.p-4.flex.flex-col.gap-4
+       (->> page-data
+            (filter #(< (:project-completion (val %)) 100))
+            (map
+             (fn [x]
+               [:li
+                [:button.bg-slate-800.py-4.rounded-lg.w-full
+                 {:onclick (format "window.location.href='/%s'" (key x))}
+                 [:h3.text-center.m-0 (:name (val x))]
+                 [:div.preview.line-clamp-2.overflow-auto
+                  {:inert ""}
+                  (pages (format "/%s/index.html" (key x)))]]])))]]]]))
 
 (defn get-pages []
-  (println page-data)
-  (stasis/merge-page-sources
-   {:index {"/index.html" index-page}
-    :public (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
-    :partials (partial-pages (stasis/slurp-directory "resources/partials" #".*\.html$"))}))
+  (let [dirs (seq (.list (io/file "./resources/partials/")))
+        page-data (->> (zipmap dirs
+                               (map #(slurp (str "./resources/partials/" % "/data.json")) dirs))
+                       (map #(first {(key %) (cheshire/parse-string (val %) true)})))]
+    (println page-data)
+    (stasis/merge-page-sources
+     {:index {"/index.html" (index-page page-data (stasis/slurp-directory "resources/partials" #".*\.html$"))}
+      :public (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
+      :partials (partial-pages (stasis/slurp-directory "resources/partials" #".*\.html$"))})))
 
 (def app (stasis/serve-pages get-pages))
