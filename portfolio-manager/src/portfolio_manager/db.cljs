13d8d95d6bc030d97d5f2d2852060f7a26f00885
WatIzDat <lily3575@hotmail.com>
2024-09-07 18:07:09 -0700

    Fix bug where article order not existing would cause failure

--- portfolio-manager/src/portfolio_manager/db.cljs
+++ portfolio-manager/src/portfolio_manager/db.cljs
@@ -18,18 +18,27 @@
 
     (.setItem js/localStorage ls-key (.stringify js/JSON (clj->js new-article)))))
 
-(defn set-article-order-local-storage [newest-id]
-  (let [ls-key (str local-storage-key local-storage-internal-separator "article-order")
-        item (.parse js/JSON (.getItem js/localStorage ls-key))]
-    (if (nil? item)
-      (.setItem js/localStorage ls-key (.stringify js/JSON (clj->js [newest-id])))
-      (.setItem js/localStorage ls-key (.stringify js/JSON (clj->js (cons newest-id (js->clj item))))))))
+(defn set-article-order-local-storage
+  ([]
+   (let [ls-key (str local-storage-key local-storage-internal-separator "article-order")]
+     (.setItem js/localStorage ls-key (.stringify js/JSON (clj->js [])))))
+  ([newest-id]
+   (let [ls-key (str local-storage-key local-storage-internal-separator "article-order")
+         item (.parse js/JSON (.getItem js/localStorage ls-key))]
+     (if (nil? item)
+       (.setItem js/localStorage ls-key (.stringify js/JSON (clj->js [newest-id])))
+       (.setItem js/localStorage ls-key (.stringify js/JSON (clj->js (cons newest-id (js->clj item)))))))))
 
 (defn get-article-from-local-storage [id]
   (js->clj (.parse js/JSON (.getItem js/localStorage (str local-storage-key local-storage-article-separator id))) :keywordize-keys true))
 
 (defn get-article-order-from-local-storage []
-  (js->clj (.parse js/JSON (.getItem js/localStorage (str local-storage-key local-storage-internal-separator "article-order")))))
+  (let [article-order (.getItem js/localStorage (str local-storage-key local-storage-internal-separator "article-order"))]
+    (if article-order
+      (js->clj (.parse js/JSON article-order))
+      (do
+        (set-article-order-local-storage)
+        []))))
 
 (defn remove-article-from-local-storage [id]
   (.removeItem js/localStorage (str local-storage-key local-storage-article-separator id)))
